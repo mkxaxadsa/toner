5393e9c4720ed7bcbe4f569ce9b6a2e8f1e15347
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-09-12 13:10:50 +0200

    fix

--- lib/core/data/news_data.dart
+++ lib/core/data/news_data.dart
@@ -2,73 +2,67 @@ import 'package:finance_app/data/models/news_item_model.dart';
 
 final List<NewsItemModel> news = [
   NewsItemModel(
-    title: "Global Markets Surge Amid Tech Stock Rally",
-    text:
-        "In a surprising turn of events, global markets surged today driven by a significant rally in tech stocks. Analysts attribute this growth to positive earnings reports from major tech companies and increased investor confidence. This rally is expected to impact future investment strategies, especially in the technology sector, as more investors look to capitalize on the momentum.",
-    imageUrl: "https://media.gq-magazine.co.uk/photos/5e25d00550c26e0008a9b030/master/pass/20200120-invest.jpg",
-    creationDate: DateTime.now(),
-  ),
-  NewsItemModel(
-    title: "Crypto Market Experiences Unprecedented Volatility",
-    text:
-        "The cryptocurrency market has been experiencing extreme volatility over the past few weeks, with Bitcoin and Ethereum seeing massive price swings. Experts believe this could be the result of increased regulatory scrutiny, as well as market manipulation by large stakeholders. Investors are advised to exercise caution and consider diversifying their portfolios to mitigate potential risks.",
-    imageUrl: "https://cloudfront-us-east-2.images.arcpublishing.com/reuters/D64WDHFFQRGX7MEOT43ZIHEGSE.JPG",
-    creationDate: DateTime.now().subtract(Duration(days: 1)),
-  ),
-  NewsItemModel(
     title: "Real Estate Investment: A Safe Haven in Uncertain Times",
     text:
         "As financial markets remain unpredictable, many investors are turning to real estate as a stable and reliable investment option. The demand for both residential and commercial properties has seen a significant rise, with experts suggesting that real estate could provide a buffer against market downturns. This trend is expected to continue as more investors seek to secure their financial future.",
-    imageUrl: "https://www.nj.com/personal-finance/static/20576be8a9d5f83f0d6d31c851654ef0/48efe/how-to-invest-in-real-estate.jpg",
+    imageUrl:
+        "https://www.nj.com/personal-finance/static/20576be8a9d5f83f0d6d31c851654ef0/48efe/how-to-invest-in-real-estate.jpg",
     creationDate: DateTime.now().subtract(Duration(days: 2)),
   ),
   NewsItemModel(
     title: "Federal Reserve Hints at Possible Interest Rate Hike",
     text:
         "The Federal Reserve has hinted at a possible interest rate hike in the near future, sparking discussions among investors about the potential impact on the economy. Higher interest rates could lead to increased borrowing costs, which might slow down economic growth. Investors are advised to monitor the situation closely and adjust their strategies accordingly to navigate the possible changes in the financial landscape.",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbocApxGlcSnV3HcHUiqM9mz9CxqsXTHvDig&s",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbocApxGlcSnV3HcHUiqM9mz9CxqsXTHvDig&s",
     creationDate: DateTime.now().subtract(Duration(days: 3)),
   ),
   NewsItemModel(
     title: "Gold Prices Rise as Investors Seek Safe Havens",
     text:
         "Amid ongoing economic uncertainty, gold prices have seen a steady increase as investors flock to this traditional safe-haven asset. The demand for gold is being driven by concerns over inflation, geopolitical tensions, and fluctuating stock markets. Financial experts suggest that investing in gold could provide a hedge against potential economic downturns, offering a layer of security for long-term investors.",
-    imageUrl: "https://media.graphassets.com/output=format:webp/resize=width:800/quality=value:75/JKFT8XtCSua6cMiuCxku",
+    imageUrl:
+        "https://media.graphassets.com/output=format:webp/resize=width:800/quality=value:75/JKFT8XtCSua6cMiuCxku",
     creationDate: DateTime.now().subtract(Duration(days: 4)),
   ),
   NewsItemModel(
     title: "Emerging Markets Offer Promising Investment Opportunities",
     text:
         "Emerging markets are increasingly attracting the attention of investors looking for high-growth opportunities. Countries in Asia, Latin America, and Africa are experiencing rapid economic development, making them attractive destinations for investment. However, these markets also come with higher risks, so investors are encouraged to conduct thorough research and consider the long-term potential before committing funds.",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTImzJsQCIavutHocQdoYwMUB3zN4W3jlBRw&s",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTImzJsQCIavutHocQdoYwMUB3zN4W3jlBRw&s",
     creationDate: DateTime.now().subtract(Duration(days: 5)),
   ),
   NewsItemModel(
     title: "Tech Innovations Drive Growth in the Fintech Sector",
     text:
         "The fintech sector is witnessing unprecedented growth, driven by technological innovations such as blockchain, artificial intelligence, and big data. These advancements are transforming the financial services industry, offering new and efficient ways to manage money, invest, and access financial products. Investors are increasingly interested in fintech startups, seeing them as key players in the future of finance.",
-    imageUrl: "https://cfotech.co.uk/uploads/story/2023/10/07/GettyImages-1202705835.webp",
+    imageUrl:
+        "https://cfotech.co.uk/uploads/story/2023/10/07/GettyImages-1202705835.webp",
     creationDate: DateTime.now().subtract(Duration(days: 6)),
   ),
   NewsItemModel(
     title: "Sustainable Investments Gain Momentum Among Millennials",
     text:
         "Sustainable investments are gaining significant traction, particularly among millennial investors who prioritize environmental, social, and governance (ESG) factors in their financial decisions. This trend is reshaping the investment landscape, as more companies adopt sustainable practices to attract eco-conscious investors. Experts predict that sustainable investments will continue to grow, offering both financial returns and positive social impact.",
-    imageUrl: "https://inyova.ch/wp-content/uploads/2023/02/Sustainable-Investing_Impact-Investing-1@2x.png",
+    imageUrl:
+        "https://inyova.ch/wp-content/uploads/2023/02/Sustainable-Investing_Impact-Investing-1@2x.png",
     creationDate: DateTime.now().subtract(Duration(days: 7)),
   ),
   NewsItemModel(
     title: "AI and Automation Transforming the Financial Sector",
     text:
         "Artificial intelligence (AI) and automation are revolutionizing the financial sector, from automated trading systems to AI-driven financial advisory services. These technologies are enhancing efficiency, reducing costs, and offering personalized investment solutions. As AI continues to evolve, its impact on the financial industry is expected to grow, making it a critical area of focus for investors and financial institutions alike.",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcSvLnqqGnPVsKKZ8Q82rMU7mvLemHgMmk0w&s",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcSvLnqqGnPVsKKZ8Q82rMU7mvLemHgMmk0w&s",
     creationDate: DateTime.now().subtract(Duration(days: 8)),
   ),
   NewsItemModel(
     title: "Renewable Energy Investments: A Bright Future Ahead",
     text:
         "Investments in renewable energy are on the rise, driven by the global shift towards sustainability and clean energy solutions. Solar, wind, and other renewable energy sources are becoming increasingly cost-competitive, attracting significant investor interest. As the world continues to combat climate change, renewable energy investments are expected to play a crucial role in shaping a sustainable future and delivering long-term financial returns.",
-    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTESRHNjs4m1DyaczkiZK8MFKylDBALNagXsA&s",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTESRHNjs4m1DyaczkiZK8MFKylDBALNagXsA&s",
     creationDate: DateTime.now().subtract(Duration(days: 9)),
   ),
 ];
